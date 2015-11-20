21eefe42b77c9c3a4cbbffca84c0ecd42e17d87e
talbor49 <talbor49@gmail.com>
2015-11-20 03:07:36 +0200

    Added auto email sending bot Fixed gitignore
    Made app.js do bin/www's job.
    
    Deleted www

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 bogrim
+
+Make sure you run app.js an not bin/www, configure webstorm that way, give it the app.js as the javascript file
--- routes/register.js
+++ routes/register.js
@@ -5,4 +5,51 @@ router.get('/', function(req, res, next) {
     res.render('register', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next) {
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+    var about = req.body.about;
+
+    var nodemailer = require('nodemailer');
+
+
+    // create reusable transporter object using SMTP transport
+    var transporter = nodemailer.createTransport("SMTP", {
+        service: 'Gmail',
+        auth: {
+            user: process.env.email_username,
+            pass: process.env.email_password
+        }
+    });
+
+    // NB! No need to recreate the transporter object. You can use
+    // the same transporter object for all e-mails
+
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+        from: 'Registration-Bot<' + process.env.email_username + '>', // sender address
+        to: 'Tal<talbor49@gmail.com>', // list of receivers
+        subject: firstName + ' ' + lastName + ' wants to join Yedidey Hakfar!', // Subject line
+        text: 'Hello, a new registrant just signed up to Yedidey Hakfar!' +
+                '\nTheir application was: ' +
+                '\nFirst name: ' + firstName + // plaintext body
+                '\nLast name: ' + lastName +  // plaintext body
+                '\nEmail: ' + email +    // plaintext body
+                '\nMore about them: ' + about      // plaintext body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error) {
+            return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+
+    });
+
+
+    res.render('index', { title: 'Express' });
+});
+
 module.exports = router;
