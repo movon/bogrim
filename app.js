4b159627a0b6dc9d97d64fee983ee0ceff9d7217
RoyChuChun <roy.shulman@gmail.com>
2015-11-26 23:37:08 +0200

    added stuff and some more stuff

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var register = require('./routes/register');
 var users = require('./routes/users');
+var login = require('./routes/login');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/register', register);
 app.use('/users', users);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
