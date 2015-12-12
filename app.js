cbd78a1fff9151857537b3e3ee233266b99a6bc8
RoyChuChun <roy.shulman@gmail.com>
2015-12-13 00:38:34 +0200

    started working on users table

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.use('/register', register);
 app.use('/users', users);
 app.use('/confirm', confirm);
 app.use('/login', login);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  /*if(req.session.firstName === undefined){
+    res.redirect('/');
+  }*/
+  //else{
+    var connection = mysql.createConnection({
+          host: process.env.sqlhost,
+          port: process.env.sqlport,
+          user: process.env.sqlusername,
+          password: process.env.sqlpassword,
+          database: process.env.dbname
+        }
+    );
+    var queryString = 'SELECT * FROM ' + process.env.dbname + '.' + process.env.tablename;
+    connection.query(queryString, function(err, rows){
+      if(!err){
+        var userTable = '<thead><tr><th>שם פרטי</th><th>שם משפחה</th><th>אימייל</th></tr></thead>';
+        for(var i = 0;i < rows.rows.length; i++){
+          userTable += '<tr>';
+          for(var key in rows.rows[i]){
+            userTable += '<td>'+ rows.rows[i][key] +'</td>';
+          }
+          userTable += '</tr>'
+        }
+      }
+    });
+    res.render('users', { title: 'express', userTable: userTable });
+  //}
 });
 
 module.exports = router;
